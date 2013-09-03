3116e6dbc1feaeefb62f51ddb06a283c8d3bbd75
jcarrharris <joncarrharris@gmail.com>
2013-09-03 16:14:09 -0400

    added photos

--- LCBO/html_generator.rb
+++ LCBO/html_generator.rb
@@ -1,7 +1,7 @@
 require 'json'
 require 'open-uri'
 
-class HtmlGenerator
+class HTMLGenerator
 	def index
 		puts "HTMLGenerator: index"
 		raw_response = open("http://lcboapi.com/products").read
@@ -17,5 +17,24 @@ class HtmlGenerator
 
 	def show(product_id)
 		# write the same as the index method but passing a product_id in		
+		puts "HTMLGenerator: products"
+		raw_response = open("http://lcboapi.com/products/300681").read
+
+		#Parse JSON-formatted text into a Ruby Hash
+		parsed_response = JSON.parse(raw_response)
+
+		#Return the actual result data from the response, ignoring metadata
+		product = parsed_response["result"]
+
+		puts product
 	end
+
+	def print_header
+
+	end
+
+	def print_footer
+	
+	end
+
 end
