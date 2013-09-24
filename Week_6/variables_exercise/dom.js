22c4f9f96dfc9a304d5acfa793c92b9165c51950
jcarrharris <joncarrharris@gmail.com>
2013-09-24 15:27:19 -0400

    added assignment

---
+++ Week_6/variables_exercise/dom.js
@@ -0,0 +1,9 @@
+var book = {
+	title: ["1984", "Prisoner of Birth", "Game of Thrones"],
+	author:["George Orwell", "Jeffrey Archer", "George Martin"],
+	alreadyRead: ["true", "true", "true"]
+};
+
+console.log(book.title[0] + " by "+ book.author[0]);
+console.log(book.title[1] + " by "+ book.author[1]);
+console.log(book.title[2] + " by "+ book.author[2]);
