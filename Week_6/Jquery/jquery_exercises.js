6099c7037dbd842c41be33d5c14ecfba2009c79d
jcarrharris <joncarrharris@gmail.com>
2013-09-25 10:34:35 -0400

    added jquery exercises

--- Week_6/Jquery/jquery_exercises.js
+++ Week_6/Jquery/jquery_exercises.js
@@ -55,14 +55,14 @@ $(document).ready(function(){
 // Move "#child" from "#parent1" to "#parent2"
 // Hint: you can use .appendTo
 
-$('#parent1').appendTo("#parent2");
+//$('#parent1').appendTo("#parent2");
 
 // EXERCISE: Select A Option in A Select Box
 
 // Select the second option in the selectbox by script
 // Hint: you will have to select the option and can use .eq and .attr
 
-
+$('#target select:eq(2)').attr("selected","selected");
 
 
 
@@ -71,40 +71,72 @@ $('#parent1').appendTo("#parent2");
 // Make "#target" double size
 // Hint: you can use .css, .width and .height
 
+// $('#target').css({
+// 	'width' = 2
+// 	 'height' = 2
+// });
+
+
+
 // EXERCISE: Empty Elements
 // Hint: you can use .empty
 
 // Remove all children and text of "#target"
+
+//$('div.child').empty();
+//$('div#target').empty();
+
+
 
 // EXERCISE: Delay
 
 // Show Alert with 1 second delay (Use "setTimeout")
 // Hint: you can use .setTimeout and alert
 
+// setTimeout(function delay() {
+// 	alert('Your site is overdone') {
+// 		$(document).fadeIn();}, 2000);
+// 		});
+
+
 // EXERCISE: Count
 
 // Show the number of children in an alert
 // Hint: you can use .children, .length and alert or you could use .find, .size and alert. the alert should show 5
+
+// var n = $('#target').children.length;
+// alert("There are " + n + " divs");
+
 
 // EXERCISE: Animate
 
 // Make "#target" double size with animation
 // Hint: you can use .animate, .width, and .height
 
+
+// $('#target').animate({width: '+=200'}, 200);
+
 // EXERCISE: Alternate Color
 
 // Make the list-box alternating. (Make odd options a different color)
 // Hint: you can select the option and use .filter and .css
+
+// $('#target .child').filter(":even").css("background-color","red");
 
 // EXERCISE: All But One
 
 // Remove all children of "#target" but h2
 // Hint: you can use .children, .not and .remove
 
+// $("#target").children().not("h2").remove();
+
 // EXERCISE: Without Children
 
 // Remove all 'div' whitch has no child elements.
 // Hint: you can use .not and .remove. Look closely at what options you can pass .not
+
+$('div:empty').remove(); 
+
 
 
 });
